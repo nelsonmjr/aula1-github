1174820b5ded53fe55eb23fb365107babf920ffd
nelsonmjr <nelsonmjr@gmail.com>
2021-11-14 10:44:28 -0300

    Alteracao de temperatura em garus Celsius

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		// Alteração de temperatura em graus Celsius
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		char resp;
